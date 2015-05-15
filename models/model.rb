2a9312d0ad031a58ea21bb46efd4807b51726575
l-a-i-n <lain@lain.org.uk>
2015-05-15 04:48:13 +0100

    models should be table-based

--- models/model.rb
+++ models/model.rb
@@ -2,7 +2,7 @@ require 'mysql2'
 require 'json'
 
 class Model
-  def initialize(config)
+  def initialize(config, table)
     data = File.read(config)
     data = JSON.parse(data)
     @core = Mysql2::Client.new(
@@ -11,8 +11,12 @@ class Model
       :password => data['password'],
       :database => data['database']
     )
+
+    @table = table
   end
 
+  # override the table and
+  # just execute the query
   def query(txt)
     @core.query(txt)
   end
